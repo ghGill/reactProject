69e0034baaa0eeebd28364f205938482e28fdcb1
Gil <gil190267@gmail.com>
2025-06-20 17:33:53 +0300

    fix config file

--- playwright.config.js
+++ playwright.config.js
@@ -14,9 +14,6 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  use: {
-    headless: false
-  },
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -34,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    headless: false,
   },
 
   /* Configure projects for major browsers */
